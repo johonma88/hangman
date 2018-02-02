b145135e562d9dd5f673994dd325bdf2af99e621
Johanna Honma <johonma@hotmail.com>
2018-02-01 20:25:05 -0700

    hangman

---
+++ game.js
@@ -0,0 +1,8 @@
+// file will randomly select a word for the player.
+
+
+
+var listOfProgrammingLanguages = ['javascript','swift','basic','fortran','pascal','apl','lisp','snobol','applescript','bash','cayenne','cobol','php','phyton'];
+ 
+// Export the list of words to other hangman script(s)
+module.exports = listOfProgrammingLanguages;
